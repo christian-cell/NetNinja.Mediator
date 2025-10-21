fbb4852188dbe8b18fb364b4d84a3c69e752a8e1
Christian García Martín <christian.garcia@plainconcepts.com>
2025-10-21 12:40:25 +0200

    adding cancellation token

--- src/NetNinja.Mediator/Services/MediatorService.cs
+++ src/NetNinja.Mediator/Services/MediatorService.cs
@@ -18,12 +18,14 @@ namespace NetNinja.Mediator.Services
         public async Task<TResponse> Send<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
         {
             var token = _httpContextAccessor.HttpContext?.RequestAborted ?? cancellationToken;
-
             var handlerType = typeof(IRequestHandler<,>).MakeGenericType(request.GetType(), typeof(TResponse));
             var handler = _serviceProvider.GetService(handlerType);
+            
             if (handler == null) throw new InvalidOperationException("Handler not found");
+            
             var method = handlerType.GetMethod("Handle");
             if (method == null) throw new InvalidOperationException("Handle method not found");
+            
             try
             {
                 var task = (Task<TResponse>)method.Invoke(handler, new object[] { request, token })!;
